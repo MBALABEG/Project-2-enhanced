f5f32e6249e9eba77da5e3b368e594541aeeb663
Melvin <mbalabeg@yahoo.com>
2015-05-09 23:17:56 -0700

    cookies working with permanat login and remember

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508205921) do
+ActiveRecord::Schema.define(version: 20150510053706) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150508205921) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
 end
