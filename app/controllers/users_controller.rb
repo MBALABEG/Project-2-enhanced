5a2043afa5d0221176a94a33982c85b7cb9394a5
Melvin <mbalabeg@yahoo.com>
2015-05-09 22:22:17 -0700

    Login Logout working through sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Vendor Directory!"
+      log_in @user
+      flash[:success] = "Welcome to the Bridal directory!"
       redirect_to @user
     else
       render 'new'
@@ -19,6 +20,7 @@ class UsersController < ApplicationController
   end
 
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
