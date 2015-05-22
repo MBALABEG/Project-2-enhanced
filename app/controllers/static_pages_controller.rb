9dfe08666365a4446ffa34c5df233349b70fa621
Melvin <mbalabeg@yahoo.com>
2015-05-22 11:51:09 -0700

    added more posts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,15 @@
 class StaticPagesController < ApplicationController
 
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
   end
 
-  def contact
-
+  def about
   end
 
-  def about
+  def contact
   end
 end
